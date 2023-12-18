332b1b049f33337e2d2000e926baf5c62b956da2
Suraj Shetty <shetty44444@gmail.com>
2023-12-18 12:28:21 +0530

    db structure changed

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/go-redis/redis"
@@ -11,7 +12,7 @@ var Ctx = context.Background()
 
 func CreateRedisClient(dbNo int) *redis.Client {
 	redis_conn := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("DB_ADDR"),
+		Addr:     fmt.Sprintf("%s:%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT")),
 		Password: os.Getenv("DB_PASS"),
 		DB:       dbNo,
 	})
